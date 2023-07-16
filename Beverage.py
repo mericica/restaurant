54f79a1ab492453fb01f8622f6726281c9245d85
Maria <122437454+mericica@users.noreply.github.com>
2023-07-16 20:37:40 +0300

    Delete Beverage.py

--- Beverage.py
+++
@@ -1,6 +0,0 @@
-from modelle.Dish import Dish
-
-class Beverage(Dish):
-    def __init__(self, id , name, price, alcohol_content, portion_size=350):
-        self.alcohol_content = alcohol_content
-        Dish.__init__(self, id, name, price, portion_size)
