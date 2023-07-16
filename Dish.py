c2f7c23d79d2340cb9b2de5ff0494b9c322d82e9
Maria <122437454+mericica@users.noreply.github.com>
2023-07-16 20:38:58 +0300

    Delete Dish.py

--- Dish.py
+++
@@ -1,7 +0,0 @@
-from modelle.Identifizierbar import Identifizierbar
-class Dish(Identifizierbar):
-    def __init__(self, id, name, price, portion_size=350):
-        self.name = name
-        self.portion_size = portion_size
-        self.price = price
-        Identifizierbar.__init__(self, id)
